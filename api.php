b24da14b146f2eb57aca0ec73d1670e532928d2f
Yehuda Putra Yura <145263609+yehuda0231@users.noreply.github.com>
2026-05-11 15:03:04 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,23 +1,3 @@
 <?php
-$conn = new mysqli(
-    "sql303.infinityfree.com",
-    "if0_41884666",
-    "Vyh4f1onprxJSAj",
-    "if0_41884666_absensi_db"
-);
-
-$nama  = $_POST['nama'] ?? '';
-$nim   = $_POST['nim'] ?? '';
-$kelas = $_POST['kelas'] ?? '';
-$waktu = $_POST['waktu'] ?? '';
-$mk    = $_POST['matkul_id'] ?? '';
-
-if($nama != '' && $mk != ''){
-    $sql = "INSERT INTO absensi (nama,nim,kelas,matkul_id,waktu)
-            VALUES ('$nama','$nim','$kelas','$mk','$waktu')";
-
-    echo $conn->query($sql) ? "sukses" : "gagal";
-} else {
-    echo "data tidak lengkap";
-}
+echo "API HIDUP";
 ?>
